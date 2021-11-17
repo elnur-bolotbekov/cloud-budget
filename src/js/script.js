82bb7cde397f896ce1c76f84a10b391003eb71fd
unknown <bolotbekovelnur744@gmail.com>
2021-11-17 18:25:34 +0600

    first

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+        items:1,
+loop:true,
+        autoplay:true,
+        autoplayTimeout: 10000,
+        autoplayHoverPause: true,
+
+    });
+});
